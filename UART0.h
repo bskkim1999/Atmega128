8c5c3e2313f4c16b58bcecaf34ac73a5febdd301
Kim Bo Seok <124337361+bskkim1999@users.noreply.github.com>
2023-07-10 00:26:40 +0900

    Create UART0.h

---
+++ UART0.h
@@ -0,0 +1,16 @@
+#ifndef UART0_H_
+#define UART0_H_
+
+extern "C"{
+	void UART0_init(void);
+
+	void UART0_transmit(char data);
+
+	unsigned char UART0_receive(void);
+
+	void UART0_print_string(char *str);
+
+	void UART0_print_1_byte_number(uint8_t n);
+
+	void UART0_print_float(float f);
+};
